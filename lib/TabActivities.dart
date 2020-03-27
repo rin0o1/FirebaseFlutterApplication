0ab76f98e0a2d2850e6aa723e91d961abc69a3a7
francescorinaldi <frarino1002@gmail.com>
2020-03-27 18:44:15 +0100

    Importing external firebase database files

--- lib/TabActivities.dart
+++ lib/TabActivities.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/cupertino.dart';
 
 class TabActivities extends StatefulWidget
@@ -13,6 +14,23 @@ class TabActivities extends StatefulWidget
 
 class TabActivitiesState extends State<TabActivities>
 {
+
+
+
+  int Counter=0;
+
+  sendData(){
+    DatabaseReference db = FirebaseDatabase.instance.reference().child("rino-3fbc0");
+    db.once().then((DataSnapshot snapshot){
+      Map<dynamic, dynamic> values = snapshot.value;
+      values.forEach((key,values) {
+        print(values["via"]);
+      });
+    });
+
+
+
+  }
   @override
   Widget build(BuildContext context) {
 
@@ -23,10 +41,14 @@ class TabActivitiesState extends State<TabActivities>
       child: new Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
+          Text(
+            '$Counter'
+          ),
        ],
       ),
      ),
           floatingActionButton: new FloatingActionButton(
+            onPressed: sendData,
             elevation: 50,
             hoverElevation: 50,
             splashColor: Colors.amberAccent,
