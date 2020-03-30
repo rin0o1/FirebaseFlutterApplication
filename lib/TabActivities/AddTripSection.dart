2905af3c9e3ba97208624de47f0f82aec79b7563
francescorinaldi <frarino1002@gmail.com>
2020-03-30 16:05:51 +0200

    Activity Add User Trip section Solving space management

--- lib/TabActivities/AddTripSection.dart
+++ lib/TabActivities/AddTripSection.dart
@@ -10,7 +10,7 @@ class AddTripSection extends StatefulWidget
 
   AddTripSectionState TripSectionState= new AddTripSectionState();
 
-  
+
   void ShowSection(){TripSectionState.setState((){TripSectionState.viewVisible=true;});}
 
 
@@ -25,97 +25,74 @@ class AddTripSectionState extends State<AddTripSection>
   bool viewVisible = false ;
   void HideSection(){setState((){viewVisible=false;});}
 
- @override
- Widget build(BuildContext context)
- {
-   return Visibility(
-     maintainSize: true,
-     maintainAnimation: true,
-     maintainState: true,
-     visible: viewVisible,
-     child:   new Center(
-       child:   new Container(
-         height: 400,
-         width: 400,
-         color: Colors.blueGrey,
-        child: Form(
-          key: _formKey,
+  @override
+  Widget build(BuildContext context)
+  {
+    return Visibility(
+      maintainSize: true,
+      maintainAnimation: true,
+      maintainState: true,
+      visible: viewVisible,
+        child:   new Container(
+          padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+          margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
+          height: 400,
+          width: 400,
+          color: Colors.blueGrey,
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
-              TextFormField(
-                decoration: const InputDecoration(
-                  hintText: 'Enter your email',
-                ),
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return 'Please enter some text';
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                decoration: const InputDecoration(
-                  hintText: 'Enter your email',
-                ),
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return 'Please enter some text';
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                decoration: const InputDecoration(
-                  hintText: 'Enter your email',
-                ),
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return 'Please enter some text';
-                  }
-                  return null;
-                },
-              ),
-              new Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.end,
+              Column(
                 children: <Widget>[
-                  new FlatButton.icon(
-                    icon: Icon(Icons.save),
-                    label: Text('Salva'),
-                    color: Colors.amber,
-                    textColor: Colors.black,
-                    padding: EdgeInsets.symmetric(vertical: 10, horizontal: 30),
-                    splashColor: Colors.white,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(7),
-                    ),
-                    onPressed: () {},
+                  TextFormField(
+
                   ),
-                  new FlatButton.icon(
-                    icon: Icon(Icons.close),
-                    label: Text('Cancel'),
-                    color: Colors.redAccent,
-                    textColor: Colors.black,
-                    splashColor: Colors.white,
-                    padding: EdgeInsets.symmetric(vertical: 10, horizontal: 30),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(7),
-                    ),
-                    onPressed: HideSection,
+                  TextFormField(
+
                   ),
+                  TextFormField(
+
+                  ),
+                ],
+              ),
+              Column(
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      new FlatButton.icon(
+                        icon: Icon(Icons.save),
+                        label: Text('Salva'),
+                        color: Colors.amber,
+                        textColor: Colors.black,
+                        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+                        splashColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(7),
+                        ),
+                        onPressed: () {},
+                      ),
+                      new FlatButton.icon(
+                        icon: Icon(Icons.close),
+                        label: Text('Cancel'),
+                        color: Colors.redAccent,
+                        textColor: Colors.black,
+                        splashColor: Colors.white,
+                        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(7),
+                        ),
+                        onPressed: HideSection,
+                      ),
+                    ],
+                  )
                 ],
               )
             ],
           ),
-
-
-        ),
-       ),
-
-     ),
-   );
- }
+            ),
+    );
+  }
 
 
   Widget build_(BuildContext context) {
@@ -126,44 +103,44 @@ class AddTripSectionState extends State<AddTripSection>
       visible: viewVisible,
       child:   new Center(
         child:   new Container(
-              height: 400,
-              width: 400,
-              color: Colors.blueGrey,
-                      child: new Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        crossAxisAlignment: CrossAxisAlignment.end ,
-                        children: <Widget>[
-                          new FlatButton.icon(
-                            icon: Icon(Icons.save),
-                            label: Text('Salva'),
-                            color: Colors.amber,
-                            textColor: Colors.black,
-                            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 30),
-                            splashColor: Colors.white,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(7),
-                            ),
-                            onPressed: () {},
-                          ),
-                          new FlatButton.icon(
-                            icon: Icon(Icons.close),
-                            label: Text('Cancel'),
-                            color: Colors.redAccent,
-                            textColor: Colors.black,
-                            splashColor: Colors.white,
-                            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 30),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(7),
-                            ),
-                            onPressed: HideSection,
-                          ),
-                        ],
-
-                      ),
-
-                    ),
-
+          height: 400,
+          width: 400,
+          color: Colors.blueGrey,
+          child: new Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.end ,
+            children: <Widget>[
+              new FlatButton.icon(
+                icon: Icon(Icons.save),
+                label: Text('Salva'),
+                color: Colors.amber,
+                textColor: Colors.black,
+                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+                splashColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(7),
                 ),
+                onPressed: () {},
+              ),
+              new FlatButton.icon(
+                icon: Icon(Icons.close),
+                label: Text('Cancel'),
+                color: Colors.redAccent,
+                textColor: Colors.black,
+                splashColor: Colors.white,
+                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(7),
+                ),
+                onPressed: HideSection,
+              ),
+            ],
+
+          ),
+
+        ),
+
+      ),
     );
   }
 }
